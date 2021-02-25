dfebbd289daed20d75aaa787ec8d661590cd08b1
vladimiryumang <79560433+vladimiryumang@users.noreply.github.com>
2021-02-25 13:10:57 +0800

    Add files via upload

---
+++ WordGame/Interface/IWordCountLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Interface
+{
+    public interface IWordCountLogic
+    {
+        string LookUpText { get; set; }
+
+        List<string> ParagraphList { get; set; }
+
+        void Produce();
+
+        void ConvertLookUpToParagraph();
+    }
+}
---
+++ WordGame/Interface/IWordCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Interface
+{
+    public interface IWordCounter
+    {
+        int frequency { get; set; }
+        string word { get; set; }
+        List<string> wordInParagraph { get; set; }
+
+        void FindWordInList(string strToFind, List<string> lstToSearchFrom);
+        void FindWordInList(string strToFind, string strToSearchFrom);
+    }
+}
